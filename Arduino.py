029bb106123c490f5dce229945a48fdf846b944a
Aryan Veturekar <42694579+aryanveturekar@users.noreply.github.com>
2020-07-07 17:14:01 +0530

    Update Arduino.py

--- Arduino.py
+++ Arduino.py
@@ -1,9 +1,9 @@
 from visual import *
-import serial #Serial imported for Serial communication
-import time #Required to use delay functions
+import serial # Serial imported for Serial communication
+import time # Required to use delay functions
 
-ArduinoSerial = serial.Serial('com18',9600) #Create Serial port object called arduinoSerialData
-time.sleep(2) #wait for 2 secounds for the communication to get established
+ArduinoSerial = serial.Serial('com18',9600) # Create Serial port object called arduinoSerialData
+time.sleep(2) # wait for 2 secounds for the communication to get established
 
 obj = box(pos=(-5,0,0), size=(0.1,4,4), color=color.white)
 wallL = box(pos=(-1,0,0), size=(0.2,12,12), color=color.cyan)
@@ -14,7 +14,7 @@ t = 0
 while 1:
     rate(100)
 
-    t = int (ArduinoSerial.readline()) #read the serial data and print it as line
+    t = int (ArduinoSerial.readline()) # read the serial data and print it as line
     t= t* 0.05
     obj.pos.x = t
     print(t)
